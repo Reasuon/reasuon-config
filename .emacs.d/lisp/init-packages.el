8561d17dd6074627bc2de5185ca4c84a73147059
reasuon <reasuon@gmail.com>
2021-06-23 15:33:31 +0800

    修正无法自动启动自动补全的问题，增加fuzzy模糊匹配

--- .emacs.d/lisp/init-packages.el
+++ .emacs.d/lisp/init-packages.el
@@ -1,5 +1,4 @@
 ;; 插件配置
-
 ;; 快速重启 emacs
 (use-package restart-emacs)
 
@@ -9,7 +8,22 @@
 ;; 自动补全
 (use-package auto-complete)
 (require 'auto-complete)
+;; 启动补全的筛选
+;; 启动该项后，在补全时，可以用C-s进行筛选
+(setq ac-use-menu-map t)
+;; 启动 fuzzy 模式
+(setq ac-fuzzy-enable t)
+
+;; 安装 fuzzy-el
+;; git clone https://github.com/auto-complete/fuzzy-el.git ~/.emacs.d/lisp/common/
+(add-to-list 'load-path (expand-file-name (concat user-emacs-directory "lisp/common/fuzzy-el")))
+(require 'fuzzy)
+;;使用fuzzy功能
+(setq ac-fuzzy-enable t)
+
 ;; 启动自动补全
-(auto-complete-mode 1)
+(global-auto-complete-mode t)
+;; helm 模糊查询
+(use-package helm)
 
 (provide 'init-packages)
